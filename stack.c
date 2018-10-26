ddff31e7ac668420c6adde09b8aa5ce23578613c
Kamil Szpakowski <32940567+shizzeer@users.noreply.github.com>
2018-10-26 22:06:12 +0200

    Changed tabs to spaces

--- stack.c
+++ stack.c
@@ -3,15 +3,15 @@
 
 void stack_push(struct stack *stack_state, int element)
 {
-	assert(stack_state->size_of_elements < CAPACITY);
-	
-	stack_state->elements[stack_state->size_of_elements] = element;
-	stack_state->size_of_elements++;
+    assert(stack_state->size_of_elements < CAPACITY);
+    
+    stack_state->elements[stack_state->size_of_elements] = element;
+    stack_state->size_of_elements++;
 }
 
 void stack_pop(struct stack *stack_state, int *relocation_site)
 {
-	assert(stack_state->size_of_elements > 0);
+    assert(stack_state->size_of_elements > 0);
 
-	*relocation_site = stack_state->elements[--stack_state->size_of_elements];
+    *relocation_site = stack_state->elements[--stack_state->size_of_elements];
 }
